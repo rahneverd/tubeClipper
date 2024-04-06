4754671c81ea3b358e3ce56b0b5c3f8481ed11a0
rahneverd <rahneverd@gmail.com>
2024-04-06 13:19:10 -0700

    init

---
+++ script.js
@@ -0,0 +1,21 @@
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "copyToClipboard") {
+    const variableValue = message.videoURL;
+
+    function copyToClipboard(text) {
+      navigator.clipboard.writeText(text).then(
+        () => {
+          console.log("Text successfully copied to clipboard");
+          console.log('oas ' + text)
+        },
+        (err) => {
+          console.error("Failed to copy text to clipboard", err);
+        }
+      );
+    }
+
+    // Copy the received value to clipboard
+    copyToClipboard(variableValue);
+  }
+});
